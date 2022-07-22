8cfd432aeb241e27f599c0880d0348c2fad5f71b
Tabitha <spencersalmon.tabitha@gmail.com>
2022-07-21 22:33:56 -0400

    make sure file is up to date

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-// Worked with Joshua Rae on lines 3-13
+// Study group with Joshua Rea
 var generateBtn = document.querySelector('#generate');
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
